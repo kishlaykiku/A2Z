f7f109efcc34a9a41ebb9241ab0139031585cb28
kishlaykiku <kishlaykumarkkp24@gmail.com>
2025-12-08 22:53:50 +0530

    boiler

---
+++ Basics/Patterns/Pattern19.java
@@ -0,0 +1,28 @@
+/*
+**********
+****  ****
+***    ***
+**      **
+*        *
+*        *
+**      **
+***    ***
+****  ****
+**********
+[stars, space, stars] INVERT [stars, space, stars]
+*/
+
+public class Pattern19 {
+
+    void solution(int n) {
+
+    }
+
+    public static void main(String args[]) {
+
+        Pattern19 obj = new Pattern19();
+        int n = 5;
+
+        obj.solution(n);
+    }
+}
