09f23a49b68abeac3485c647c185f0d263971c40
kishlaykiku <kishlaykumarkkp24@gmail.com>
2025-12-11 19:46:41 +0530

    Pattern 21

--- Basics/Patterns/Pattern21.java
+++ Basics/Patterns/Pattern21.java
@@ -11,6 +11,15 @@ public class Pattern21 {
 
     void solution(int n) {
 
+        for(int i = 0; i < n-1; i++) {
+
+            for(int j = 0; j < n-1; j++) {
+                
+                if(i == 0 || j == 0 || i == n-2 || j == n-2) System.out.print("*");
+                else System.out.print(" ");
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String args[]) {
