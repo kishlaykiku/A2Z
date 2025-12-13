4a41d131d703df84b64cdbfbe39038d9a8f50843
kishlaykiku <kishlaykumarkkp24@gmail.com>
2025-12-13 11:56:27 +0530

    Pattern 22

--- Basics/Patterns/Pattern22.java
+++ Basics/Patterns/Pattern22.java
@@ -15,6 +15,19 @@ public class Pattern22 {
 
     void solution(int n) {
 
+        for(int i = 0; i < (n*2)-1; i++) {
+
+            for(int j = 0; j < (n*2)-1; j++) {
+
+                int top = i;
+                int bottom = ((n*2)-2)-i;
+                int left = j;
+                int right = ((n*2)-2)-j;
+
+                System.out.print(n-Math.min(Math.min(top, bottom), Math.min(left, right)) + " ");
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String args[]) {
