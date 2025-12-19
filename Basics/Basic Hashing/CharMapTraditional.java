9ed14253c028dc8c52c63eff12c41872f7e517ea
kishlaykiku <kishlaykumarkkp24@gmail.com>
2025-12-19 15:21:00 +0530

    Traditional Character hashing

---
+++ Basics/Basic Hashing/CharMapTraditional.java
@@ -0,0 +1,41 @@
+/*
+Problem Statement: Count the frequency of characters
+
+Example =>
+    Input: str = "abcdabefc" || inpSize = 4 || charCheck = a, c, f, z
+    Output: 2 2 1 0
+    Explanation: The frequency of a, c, f, z in arr is 2, 2, 1, 0.
+*/
+
+import java.util.Scanner;
+
+public class CharMapTraditional {
+
+    private static Scanner sc = new Scanner(System.in);
+
+    void solution(char charCheck, String str) {
+
+        char start = 'a';
+        int[] freq = new int[26];
+
+        for(int i = 0; i < str.length(); i++) {
+            freq[str.charAt(i)-start]++;
+        }
+
+        System.out.print(freq[charCheck - start]);
+    }
+
+    public static void main(String args[]) {
+
+        CharMapTraditional obj = new CharMapTraditional();
+        String str = sc.nextLine();
+        int inpSize = sc.nextInt();
+
+        for(int i = 0; i < inpSize; i++) {
+            char charCheck = sc.next().charAt(0);
+            obj.solution(charCheck, str);
+            if(i < inpSize-1)
+                System.out.print(" ");
+        }
+    }
+}
