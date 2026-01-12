650f128139440ef367ae6d2eda1dc13576f310d4
kishlaykiku <kishlaykumarkkp24@gmail.com>
2026-01-12 22:10:22 +0530

    Bubble Sort Boiler

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -1,37 +1,26 @@
 /*
-In Selection Sort, we select the minimum element and start placing them from the beginning of the unsorted array. 
+In Bubble Sort, we push the maximum element to the end of the array by adjacent swapping.
 Time Complexity =>
-    Best: [O(n^2)]
-    Average: [O(n^2)]
-    Worst: [O(n^2)]
+    Best: 
+    Average: 
+    Worst: 
 */
 
 import java.util.Scanner;
 import java.util.List;
 import java.util.ArrayList;
 
-public class SelectionSort {
+public class BubbleSort {
 
     private static Scanner sc = new Scanner(System.in);
 
     void solution(List<Integer> arr) {
 
-        for(int i = 0; i < arr.size()-1; i++) {
-            int minIndex = i;
-            for(int j = i+1; j < arr.size(); j++) {
-                if(arr.get(j) < arr.get(minIndex)) {
-                    minIndex = j;
-                }
-            }
-            int temp = arr.get(i);
-            arr.set(i, arr.get(minIndex));
-            arr.set(minIndex, temp);
-        }
     }
 
     public static void main(String args[]) {
 
-        SelectionSort obj = new SelectionSort();
+        BubbleSort obj = new BubbleSort();
         List<Integer> arr = new ArrayList<>();
 
         while(sc.hasNext()) {
