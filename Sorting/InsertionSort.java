0d84b41c5133464564e44d485d2181ffe311c93c
kishlaykiku <kishlaykumarkkp24@gmail.com>
2026-01-20 10:19:35 +0530

    Insertion Sort Boiler

---
+++ Sorting/InsertionSort.java
@@ -0,0 +1,47 @@
+/*
+In Insertion Sort, we take an element and place it in the correct order.
+Time Complexity =>
+    Best: 
+    Average: 
+    Worst: 
+*/
+
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
+public class InsertionSort {
+
+    private static Scanner sc = new Scanner(System.in);
+
+    void solution(List<Integer> arr) {
+
+    }
+
+    public static void main(String args[]) {
+
+        InsertionSort obj = new InsertionSort();
+        List<Integer> arr = new ArrayList<>();
+
+        while(sc.hasNext()) {
+            if(sc.hasNextInt()) {
+                int number = sc.nextInt();
+                arr.add(number);
+            } else {
+                String input = sc.next();
+                if(input.equalsIgnoreCase("q")) // When input is "q", loop will stop
+                    break;
+            }
+        }
+        sc.close();
+
+        obj.solution(arr);
+
+        // Output
+        for(int i = 0; i < arr.size(); i++) {
+            System.out.print(arr.get(i));
+            if(i < arr.size()-1)
+                System.out.print(" ");
+        }
+    }
+}
